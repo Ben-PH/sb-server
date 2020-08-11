ca797fcfc05b4ed5771c2eaa11a6a3b3236611b1
Ben-PH <benphawke@gmail.com>
2020-08-11 05:22:43 +0200

    Refactor code into modules

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,24 @@
-use std::num::NonZeroU32;
 
+use ring::rand::SystemRandom;
 use actix_files::{Files, NamedFile};
 use actix_identity::{CookieIdentityPolicy, Identity, IdentityService};
 use actix_web::{
-    delete, get, middleware, post, web, App, HttpResponse, HttpServer, Responder, Result,
+    middleware, web, App, HttpServer, Result,
 };
-use rand::Rng;
-use ring::rand::{SecureRandom, SystemRandom};
 
-use mongodb::bson::{self, doc, spec, Binary};
+mod auth;
+pub mod shared;
+use rand::Rng;
+use ring::rand::SecureRandom;
+
+use mongodb::bson::{self, doc};
 use serde::{Deserialize, Serialize};
 
-static PBKDF2_ALG: ring::pbkdf2::Algorithm = ring::pbkdf2::PBKDF2_HMAC_SHA256;
-const CREDENTIAL_LEN: usize = ring::digest::SHA256_OUTPUT_LEN;
-const VER_ITER: u32 = 1000;
-#[derive(Serialize)]
-pub struct Credential([u8; CREDENTIAL_LEN]);
 // static PWD_DB_SALT: &[u8; 16] = b"database spicey!";
 
-struct DbCollections {
-    users: mongodb::Collection,
-}
 
-struct PwdDb {
-    pbkdf2_iters: NonZeroU32,
-    // TODO? include a db-wide salt
-    storage: mongodb::Collection,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct AuthData {
-    email: String,
-    password: String,
+pub struct AppData {
+    rng: SystemRandom,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -43,214 +30,11 @@ struct UserDocument {
     password: String,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-struct User {
-    first_name: String,
-    last_name: String,
-    email: String,
-    password: String,
-}
-
 async fn index(id: Identity) -> Result<NamedFile> {
     println!("your id is {:?}", id.identity());
     Ok(NamedFile::open("./client/index.html")?)
 }
 
-impl PwdDb {
-    /// See [this stack-overflow](https://stackoverflow.com/a/674931) question on salting
-    fn fill_salt(
-        &self,
-        rng: &SystemRandom,
-        barray: &mut [u8],
-    ) -> Result<(), ring::error::Unspecified> {
-        rng.fill(barray)
-    }
-}
-
-#[post("/create")]
-async fn create(
-    auth_data: web::Json<User>,
-    app_data: web::Data<AppData>,
-    user_db: web::Data<DbCollections>,
-    pswd_db: web::Data<PwdDb>,
-) -> Result<impl Responder> {
-    let user_db = user_db.into_inner();
-    let auth_data = auth_data.into_inner();
-    if let Ok(Some(_)) = user_db
-        .users
-        .find_one(doc! {"email": &auth_data.email}, None)
-        .await
-    {
-        return HttpResponse::Unauthorized()
-            .reason("email already exists")
-            .await;
-    }
-
-    // TODO CRUCIAL do these the rusty way. if it returned Err, we still go ahead :(
-    let pswd_db = pswd_db.into_inner();
-    if let Ok(Some(_)) = pswd_db
-        .storage
-        .find_one(doc! {"email": &auth_data.email}, None)
-        .await
-    {
-        return HttpResponse::InternalServerError()
-            .reason("inconsistent database")
-            .await;
-    }
-
-    let mut salt = [0u8; CREDENTIAL_LEN];
-    if pswd_db
-        .fill_salt(&app_data.into_inner().rng, &mut salt)
-        .is_err()
-    {
-        return HttpResponse::InternalServerError().await;
-    }
-    let mut to_store = Credential([0u8; CREDENTIAL_LEN]);
-    ring::pbkdf2::derive(
-        PBKDF2_ALG,
-        pswd_db.pbkdf2_iters,
-        &salt,
-        &auth_data.password.as_bytes(),
-        &mut to_store.0,
-    );
-    let hash = Binary {
-        subtype: spec::BinarySubtype::Generic,
-        bytes: to_store.0.to_vec(),
-    };
-
-    // TODO handle bad insertion
-    pswd_db
-        .storage
-        .insert_one(
-            doc! {
-                "email": &auth_data.email,
-                "hashed": &hash,
-            },
-            None,
-        )
-        .await;
-
-    // TODO handle bad insertion
-    user_db
-        .users
-        .insert_one(
-            doc!{
-                "first_name": auth_data.first_name,
-                "last_name": auth_data.last_name,
-                "email": auth_data.email,
-                // TODO: link to password hashing document
-                "salt": bson::Binary{subtype: bson::spec::BinarySubtype::Generic, bytes: salt.to_vec()}
-            },
-            None
-        ).await;
-    HttpResponse::Ok().await
-}
-
-#[post("/login")]
-async fn login(
-    id: Identity,
-    auth_data: web::Json<AuthData>,
-    passwords: web::Data<PwdDb>,
-    col: web::Data<DbCollections>,
-) -> Result<impl Responder> {
-    let auth = auth_data.into_inner();
-    let pwds = passwords.into_inner();
-    let col = col.into_inner();
-    let hash = match pwds
-        .storage
-        .find_one(doc! {"email": &auth.email}, None)
-        .await
-    {
-        Ok(Some(mut doc)) => match doc.remove("hashed") {
-            Some(bson::Bson::Binary(bin)) => {
-                println!("{:#?}", doc);
-                Some(bin.bytes)
-            }
-
-            _ => None,
-        },
-        _ => None,
-    };
-
-    let user = col.users.find_one(doc! {"email": &auth.email}, None).await;
-    let salt = match user {
-        Ok(Some(mut doc)) => match doc.remove("salt") {
-            Some(bson::Bson::Binary(bin)) => {
-                println!("{:?}", doc);
-                Some(bin.bytes)
-            }
-
-            _ => None,
-        },
-        _ => None,
-    };
-
-    // println!("{:?}", salt);
-    // println!("{:?}", hash);
-
-    // pull out the password hash and users salt from the database
-    // TODO don't do unwrap in a scop guarded by is_some(). do it the rusty way.
-    let mut user = match col.users.find_one(doc! {"email": &auth.email}, None).await {
-        Ok(Some(user)) => user,
-        Ok(None) => return HttpResponse::Unauthorized().reason("couldn't find the users email").await,
-        _ => return HttpResponse::InternalServerError().await
-    };
-
-    user.remove("_id");
-    user.remove("salt");
-    if salt.is_some()
-        && hash.is_some()
-        && ring::pbkdf2::verify(
-            PBKDF2_ALG,
-            pwds.pbkdf2_iters,
-            salt.unwrap().as_slice(),
-            auth.password.as_bytes(),
-            hash.unwrap().as_slice(),
-        )
-        .is_ok()
-    {
-        id.remember(auth.email);
-        HttpResponse::Ok().reason("you are logged in").json(user).await
-    } else {
-        HttpResponse::Unauthorized()
-            .reason("invalid username or password")
-            .await
-    }
-}
-
-#[delete("")]
-async fn logout(id: Identity) -> Result<impl Responder> {
-    match id.identity() {
-        Some(email) => {
-            println!("goodbye, {:?}", email);
-            id.forget();
-            HttpResponse::Ok().finish().await
-        }
-        None => {
-            println!("sorry, who are you?");
-            HttpResponse::Unauthorized()
-                .reason("invalid Authorization cookie")
-                .await
-        }
-    }
-}
-
-struct AppData {
-    rng: SystemRandom,
-}
-
-#[get("")]
-async fn profile(id: Identity, users: web::Data<DbCollections>) -> Result<impl Responder> {
-    println!("auth get id: {:?}", id.identity());
-    match id.identity() {
-        Some(email) => {
-            let mut user = users.into_inner().users.find_one(doc! {"email": &email}, None).await.unwrap().unwrap();
-            user.remove("_id");
-            user.remove("salt");
-            HttpResponse::Ok().json(user).await},
-        None => HttpResponse::Unauthorized().reason("no-good auth cookie").await,
-    }
-}
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
@@ -282,21 +66,13 @@ async fn main() -> std::io::Result<()> {
                     .secure(true),
             ))
             .wrap(middleware::Logger::default())
-            .data(DbCollections { users })
-            .data(PwdDb {
-                pbkdf2_iters: NonZeroU32::new(VER_ITER).unwrap(),
-                storage: pwds,
-            })
-            .data(AppData { rng })
-            .service(
-                web::scope("/api/auth")
-                    .service(create)
-                    .service(profile)
-                    .service(login)
-                    .service(logout)
-                    .default_service(web::route().to(web::HttpResponse::NotFound)),
-            )
+            .data(auth::DbCollections::init(users))
+            .data(auth::PwdDb::init(pwds))
+            .data(AppData{rng})
+            .service(web::scope("/api/auth").configure(auth::config).default_service(web::route().to(web::HttpResponse::NotFound)))
+
             .service(Files::new("/pkg", "./client/pkg"))
+            .service(Files::new("/", "./client/static").index_file("index.html"))
             .default_service(web::get().to(index))
     })
     .bind("127.0.0.1:8080")?
